0248d4fe0cc311335e3ac73ed3e8842ea747279e
TejasM <tejasmehta0@gmail.com>
2013-09-04 19:35:25 -0400

    Site Config

--- benselfies/settings.py
+++ benselfies/settings.py
@@ -161,12 +161,12 @@ LOGGING = {
     }
 }
 # #
-import dj_database_url
+# import dj_database_url
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 #Allow all host headers
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
