a1193f8d8881e9e44a31c4e2af844126dbc0cbd5
TejasM <tejasmehta0@gmail.com>
2013-09-08 16:35:55 -0400

    Media root

--- benselfies/settings.py
+++ benselfies/settings.py
@@ -62,7 +62,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = '/home/benese5/site/facebook-site/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
