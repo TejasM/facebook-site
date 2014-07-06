fc8b7292c64f2f79c6cb8e8f79d4fb0f19b65b03
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-05 21:03:00 -0400

    Update databse

--- benselfies/settings.py
+++ benselfies/settings.py
@@ -210,15 +210,15 @@ FACEBOOK_EXTENDED_PERMISSIONS = ['email', 'user_photos', 'publish_stream', 'phot
 # ALLOWED_HOSTS = ['*']
 #
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'iamtom',  # Or path to database file if using sqlite3.
-#         # The following settings are not used with sqlite3:
-#         'USER': 'tejas',
-#         'PASSWORD': 'tejas',
-#         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '',  # Set to empty string for default.
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'iamtom',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'tejas',
+        'PASSWORD': 'tejas',
+        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',  # Set to empty string for default.
+    }
+}
 
