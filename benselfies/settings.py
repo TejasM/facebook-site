d062dc9a98776b9cdffadfb3cdb38bb8842ae3bb
Tejas Mehta <tejasmehta0@gmail.com>
2014-07-09 15:41:17 -0400

    Fix to jpg

--- benselfies/settings.py
+++ benselfies/settings.py
@@ -62,7 +62,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/iamtom/facebook-site/media/'
+MEDIA_ROOT = 'media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
